83a86325dbd941745fa18743a4957200e64b92f4
Derek So <derekso.email@gmail.com>
2017-01-11 02:48:53 +0800

    added blogging function in middleman site

--- Gemfile
+++ Gemfile
@@ -8,7 +8,13 @@ gem 'wdm', '~> 0.1.0', platforms: [:mswin, :mingw]
 # Windows does not come with time zone data
 gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
 
+gem 'builder'  # For feed.xml.builder
+gem 'kramdown'  # For Markdown
+gem 'nokogiri' # For blog post summaries
+
 # Middleman Gems
 gem 'middleman', '>= 4.0.0'
 gem 'middleman-livereload'
-gem "middleman-rsync"
+gem 'middleman-rsync'
+gem 'middleman-blog', '~> 4.0'
+gem 'middleman-syntax'
--- config.rb
+++ config.rb
@@ -1,10 +1,9 @@
-###
-# Page options, layouts, aliases and proxies
-###
+Time.zone = 'UTC'
 
-# Per-page layout changes:
-#
-# With no layout
+page '/feed.xml', layout: false
+page '/sitemap.xml', layout: false
+page '/robots.txt', layout: false
+
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
@@ -34,14 +33,68 @@ end
 #   end
 # end
 
+# Markdown settings
+set :markdown_engine, :kramdown
+set :markdown,
+    layout_engine: :slim,
+    tables: true,
+    autolink: true,
+    smartypants: true,
+    input: 'GFM'
+
+# Ignore stylesheet bundle because it is handled by webpack
+ignore 'stylesheets/style'
+
 # Build-specific configuration
 configure :build do
-  # Minify CSS on build
-  activate :minify_css
+  # For example, change the Compass output style for deployment
+  # activate :minify_css
 
   # Minify Javascript on build
-  activate :minify_javascript
+  # activate :minify_javascript
+
+  # Enable cache buster
+  activate :asset_hash
+
+  # Use relative URLs
+  activate :relative_assets
+
+  # Or use a different image path
+  # set :http_prefix, "/Content/images/"
+
+  activate :gzip
 end
+
+activate :blog do |blog|
+  # This will add a prefix to all links, template references and source paths
+  # blog.prefix = "blog"
+
+  # Permalink format
+  blog.permalink = '{year}/{month}/{day}/{title}.html'
+  # Matcher for blog source files
+  blog.sources = 'posts/{year}-{month}-{day}-{title}.html'
+  blog.summary_length = 250
+  blog.default_extension = '.md'
+  blog.tag_template = 'tag.html'
+  blog.calendar_template = 'calendar.html'
+
+  # Enable pagination
+  blog.paginate = true
+  blog.per_page = 10
+  blog.page_link = 'page/{num}'
+end
+
+# Syntax highlight settings
+activate :syntax
+
+# Activate Directory Indexes
+activate :directory_indexes
+
+activate :external_pipeline,
+         name: :webpack,
+         command: build? ? '$(npm bin)/webpack --bail -p' : '$(npm bin)/webpack --watch -d --progress --color',
+         source: '.tmp/dist',
+         latency: 1
 
 activate :rsync do |rsync|
   rsync.production_server = "eduapp"
